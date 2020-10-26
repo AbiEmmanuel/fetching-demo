93c8e2d36f9a6e23c0ea4745e30b10c8cc0a049d
ldupas <louisdupas@net-c.com>
2020-10-26 20:32:04 +0100

    step 2, affichage d'une propriété grâce au state

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 
 class Pokemon extends React.Component {
+    state = {
+        Pokemons: ''
+    }
 
     getPokemons = () => {
-        fetch("https://pokeapi.co/api/v2/pokemon/25")
+        fetch("https://pokeapi.co/api/v2/pokemon/5")
         .then(res => res.json())
-        .then(res => console.log(res))
+        .then(res => this.setState({ Pokemons: res}))
+    }
+
+    componentDidMount() {
+        this.getPokemons()
     }
 
     render() {
-
-        this.getPokemons()
         return(
-            <h1>Attrapez les tous ! o/</h1>
+            <div>
+                <h1>Attrapez les tous ! o/</h1>
+                <div>
+                    {this.state.Pokemons.name}
+                </div>
+            </div>
         )
     }
 }
